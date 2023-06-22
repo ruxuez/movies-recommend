97f3a2168488c242d54c3af748a0fa1e1841b314
Ruxue Zeng <zeng_ruxue@126.com>
2023-06-22 15:01:48 +0200

    Decrease multiprocessing number to 4

--- streamlit_gallery/components/find_by_category/streamlit_app.py
+++ streamlit_gallery/components/find_by_category/streamlit_app.py
@@ -430,7 +430,7 @@ def main():
             f"**{len(list(result))} Products Found**: ... Printing images..."
         )
         captions = [row["productdisplayname"] for row in result]
-        pool = Pool(8)
+        pool = Pool(4)
         images = pool.map(get_image_from_url, [row["link"] for row in result])
         c2.image(images, width=200, caption=captions)
         data_load_state.markdown(f"**{len(list(result))} Products Found**")
--- streamlit_gallery/components/find_by_image/streamlit_app.py
+++ streamlit_gallery/components/find_by_image/streamlit_app.py
@@ -71,7 +71,7 @@ def main():
             f"**{len(list(result_by_image))} Products Found**: ... Printing images..."
         )
         captions = [row["productdisplayname"] for row in result_by_image]
-        pool = Pool(8)
+        pool = Pool(4)
         images = pool.map(get_image_from_url, [row["link"] for row in result_by_image])
         st.image(images, width=200, caption=captions)
         data_load_state.markdown(f"**{len(list(result_by_image))} Products Found**")
